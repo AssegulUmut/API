a6da118cd0e12fcf8d1e74bb2b91139f6c3c32e6
aysegulUmut <assegul.kotay@gmail.com>
2023-09-01 23:06:45 +0300

    1Eylül

--- src/test/java/Practise/C07_JsonPath.java
+++ src/test/java/Practise/C07_JsonPath.java
@@ -1,5 +1,36 @@
 package Practise;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class C07_JsonPath {
+    /*
+
+***         Send a GET Request to https://reqres.in/api/users
+              * test:
+              The status code : 200
+              The content type : application/json; charset=utf-8
+              email : george.bluth@reqres.in
+              first_name : George
+              last_name : Bluth
+              avatar : https://reqres.in/img/faces/1-image.jpg
+ */
+     @Test
+    public void test01() {
+         Response response=given().when().get("https://reqres.in/api/users");
+         response.prettyPrint();
+
+         JsonPath jsonPath=response.jsonPath();//api üzerinden gelen cevabın kalıbini, yapisini degistirebiliriz
+
+         Assert.assertEquals("george.bluth@reqres.in",jsonPath.getString("data[0].email"));
+         Assert.assertEquals("George",jsonPath.getString("data[0].first_name"));
+         Assert.assertEquals("Bluth",jsonPath.getString("data[0].last_name"));
+         Assert.assertEquals("https://reqres.in/img/faces/1-image.jpg",jsonPath.getString("data[0].avatar"));
+
+     }
 
 }
